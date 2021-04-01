170d6dfac6c3b73dab8d8c479b374e1a130b7801
Messias <messiasolimpio@hotmail.com>
2021-04-01 10:28:08 -0300

    Desafio 04 concluido, todos os testes passaram

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+
+    if (!userAlreadyExists.admin) {
+      throw new Error("You don't have admin permission!");
+    }
+
+    if (!userAlreadyExists) {
+      throw new Error("User is not registered!");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
